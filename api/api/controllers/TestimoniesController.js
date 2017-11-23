b88aab08c8bca24c260dbe77370f4c48f1c15388
Jorge Andrade <jorandradefig@gmail.com>
2017-11-23 03:00:51 -0600

    Add testimony params

--- api/api/controllers/TestimoniesController.js
+++ api/api/controllers/TestimoniesController.js
@@ -4,24 +4,65 @@
  * @description :: Server-side logic for managing testimonies
  *
  */
+
 var sanitize = require('mongo-sanitize');
 
 module.exports = {
+
   /**
    * This function save the testimony data in the database
-   * @param {string} name
-   * @param {int} age
-   * @returns {}
+   * @param {string} state
+   * @param {string} municipality
+   * @param {string} date
+   * @param {number} age
+   * @param {string} details
+   * @param {string} clinic
+   * @param {number} births
+   * @param {string} contraceptive
+   * @returns {object} res
    * @throws {}
    * @author Mitsiu <alejandro.carreno@datacivica.org>
-   * @todo Receive real parameters
+   * @todo
    **/
-  create : function(req, res){
+
+  create : function(req, res) {
     var params = sanitize(req.params.all());
 
-    TestimoniesService.create(params).then(function(result){
+    if (typeof params.state === 'undefined' || typeof params.state !== 'string') {
+      return res.status(403).send({ messsage : "state param is either undefined or is not string" });
+    }
+
+    if (typeof params.municipality === 'undefined' || typeof params.municipality !== 'string') {
+      return res.status(403).send({ messsage : "municipality param is either undefined or is not string" });
+    }
+
+    if (typeof params.date === 'undefined' || typeof params.date !== 'string') {
+      return res.status(403).send({ messsage : "date param is either undefined or is not string" });
+    }
+
+    if (typeof params.age === 'undefined' || typeof params.age !== 'number') {
+      return res.status(403).send({ messsage : "age param is either undefined or is not number" });
+    }
+
+    if (typeof params.details === 'undefined' || typeof params.details !== 'string') {
+      return res.status(403).send({ messsage : "details param is either undefined or is not string" });
+    }
+
+    if (typeof params.clinic === 'undefined' || typeof params.clinic !== 'string') {
+      return res.status(403).send({ messsage : "clinic param is either undefined or is not string" });
+    }
+
+    if (typeof params.births === 'undefined' || typeof params.births !== 'number') {
+      return res.status(403).send({ messsage : "births param is either undefined or is not number" });
+    }
+
+    if (typeof params.contraceptive === 'undefined' || typeof params.contraceptive !== 'string') {
+      return res.status(403).send({ messsage : "contraceptive param is either undefined or is not string" });
+    }
+
+    TestimoniesService.create(params).then(function(result) {
       return res.json(result);
-    }).catch(function(err){
+    }).catch(function(err) {
       sails.log.error(err);
       return res.send(err.status, err);
     });
--- api/api/services/TestimoniesService.js
+++ api/api/services/TestimoniesService.js
@@ -1,33 +1,58 @@
 var ObjectId = require('mongodb').ObjectID;
 
 var self = module.exports = {
-  create : function(params){
-    return new Promise(function (resolve, reject){
-      Testimonies.native(function (err, collection){
-        if(err){
+  create : function(params) {
+    return new Promise(function (resolve, reject) {
+      Testimonies.native(function (err, collection) {
+        if(err) {
           sails.log.error(err);
-          reject({status:500, error: true, location:"TestimoniesService.create: Native. Collection Testimonies", message:"Ocurrió un error interno, por favor intente más tarde", info:err});
+          reject({ status:500, error: true, location:"TestimoniesService.create: Native. Collection Testimonies", message:"Ocurrió un error interno, por favor intente más tarde", info:err });
         }
         var query = self.createQuery(params);
 
-        collection.insertOne(query, function(err, result){
-          if(err){
+        collection.insertOne(query, function(err, result) {
+          if(err) {
             sails.log.error(err);
-            reject({status:500, error:true, location:"TestimoniesService.create: Error in insertOne", message:"Ocurrió un error al guardar el testimonio", info:err});
+            reject({ status:500, error:true, location:"TestimoniesService.create: Error in insertOne", message:"Ocurrió un error al guardar el testimonio", info:err });
           }
           resolve(result.ops[0]);
         });
       });
     });
   },
-  createQuery : function(params){
+  createQuery : function(params) {
     var query = {};
 
-    if(params.name){
-      query.name = params.name;
+    if(params.state) {
+      query.state = params.state;
     }
-    if(params.age){
+
+    if(params.municipality) {
+      query.municipality = params.municipality;
+    }
+
+    if(params.date) {
+      query.date = params.date;
+    }
+
+    if(params.age) {
       query.age = params.age;
+    }
+
+    if(params.details) {
+      query.details = params.details;
+    }
+
+    if(params.clinic) {
+      query.clinic = params.clinic;
+    }
+
+    if(params.births) {
+      query.births = params.births;
+    }
+
+    if(params.contraceptive) {
+      query.contraceptive = params.contraceptive;
     }
 
     query.createdAt = new Date();
